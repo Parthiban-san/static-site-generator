69d9883b346150e52578e3ddfa2aa7efb59c1418
Parthiban-san <parthibanselvaraj2306@gmail.com>
2025-12-29 17:53:49 +0530

    Added delimiter

---
+++ src/delimiter.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            pass
+        else:
+            new_nodes.append(node)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from textnode import TextType, TextNode
+from htmlnode import *
 
 def main():
     textnode1 = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
-    print(textnode1)
+    htmlnode1 = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+    print(htmlnode1.to_html())
 
 if __name__ == "__main__":
     main()
